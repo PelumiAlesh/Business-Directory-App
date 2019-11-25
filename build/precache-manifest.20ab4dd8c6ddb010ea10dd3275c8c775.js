595d6cc5abdd42bc884440706fe68f2693e767ec
pelumiAlesh <aleshinloyelukony@gmail.com>
2019-11-25 00:18:28 -0800

    fix

---
+++ build/precache-manifest.20ab4dd8c6ddb010ea10dd3275c8c775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e128f3f269c0c5d4b3ddc5196bb42591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c25dc6c8df36a9f40e",
+    "url": "/static/css/main.7390e6a1.chunk.css"
+  },
+  {
+    "revision": "8e29f880ccd63d3f0312",
+    "url": "/static/js/2.015f712e.chunk.js"
+  },
+  {
+    "revision": "88c25dc6c8df36a9f40e",
+    "url": "/static/js/main.6d716708.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "3f2564f4afe83fe66942561b4cc09621",
+    "url": "/static/media/homeCover.3f2564f4.jpg"
+  }
+]);
